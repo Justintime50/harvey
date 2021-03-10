f5b44442fd6b66f953ceaa75e27e8503e7f644c5
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-03-09 23:09:24 -0700

    v0.10.0 - overhaul stages and add tests

--- test/unit/test_stages.py
+++ test/unit/test_stages.py
@@ -1,9 +1,42 @@
-from test.unit.conftest import \
-    mock_response_container  # Remove once fixtures are fixed
+import subprocess
+from test.unit.conftest import mock_config  # Remove once fixtures are fixed
+from test.unit.conftest import mock_response_container
 
 import mock
 from harvey.globals import Global
-from harvey.stages import Stage
+from harvey.stages import BuildStage, DeployComposeStage, DeployStage
+
+MOCK_OUTPUT = 'mock output'
+
+
+@mock.patch('harvey.images.Image.remove_image')
+@mock.patch('subprocess.check_output')
+def test_build_stage_success(mock_subprocess, mock_remove_image, mock_webhook):
+    # TODO: Mock the subprocess better to ensure it does what it's supposed to
+    _ = BuildStage.run(mock_config('deploy'), mock_webhook, MOCK_OUTPUT)
+
+    mock_remove_image.assert_called_once()
+    mock_subprocess.assert_called_once()
+
+
+@mock.patch('harvey.images.Image.remove_image')
+@mock.patch('harvey.utils.Utils.kill')
+@mock.patch('subprocess.check_output', side_effect=subprocess.TimeoutExpired(cmd=subprocess.check_output, timeout=0.1))  # noqa
+def test_build_stage_subprocess_timeout(mock_subprocess, mock_utils_kill, mock_remove_image, mock_project_path, mock_webhook):  # noqa
+    _ = BuildStage.run(mock_config('deploy'), mock_webhook, MOCK_OUTPUT)
+
+    mock_remove_image.assert_called_once()
+    mock_utils_kill.assert_called_once()
+
+
+@mock.patch('harvey.images.Image.remove_image')
+@mock.patch('harvey.utils.Utils.kill')
+@mock.patch('subprocess.check_output', side_effect=subprocess.CalledProcessError(returncode=1, cmd=subprocess.check_output))  # noqa
+def test_build_stage_process_error(mock_subprocess, mock_utils_kill, mock_remove_image, mock_project_path, mock_webhook):  # noqa
+    _ = BuildStage.run(mock_config('deploy'), mock_webhook, MOCK_OUTPUT)
+
+    mock_remove_image.assert_called_once()
+    mock_utils_kill.assert_called_once()
 
 
 @mock.patch('time.sleep', return_value=None)
@@ -11,7 +44,7 @@ from harvey.stages import Stage
             return_value=mock_response_container(status=200, dead=False, paused=False,
                                                  restarting=False, running=True))
 def test_run_container_healthcheck_success(mock_container_json, mock_sleep, mock_webhook):
-    healthcheck = Stage.run_container_healthcheck(mock_webhook)
+    healthcheck = DeployStage.run_container_healthcheck(mock_webhook)
 
     mock_container_json.assert_called_once_with(Global.docker_project_name(mock_webhook))
     assert healthcheck is True
@@ -22,7 +55,46 @@ def test_run_container_healthcheck_success(mock_container_json, mock_sleep, mock
             return_value=mock_response_container(status=500, dead=True, paused=False,
                                                  restarting=False, running=False))
 def test_run_container_healthcheck_failed(mock_container_json, mock_sleep, mock_webhook):
-    healthcheck = Stage.run_container_healthcheck(mock_webhook)
+    """This test checks that if a healthcheck fails, we properly retry
+    """
+    healthcheck = DeployStage.run_container_healthcheck(mock_webhook)
 
-    mock_container_json.assert_called_once_with(Global.docker_project_name(mock_webhook))
+    mock_container_json.assert_called_with(Global.docker_project_name(mock_webhook))
+    assert mock_container_json.call_count == 6
     assert healthcheck is False
+
+
+@mock.patch('subprocess.check_output')
+def test_deploy_compose_stage_success(mock_subprocess, mock_webhook):
+    # TODO: Mock the subprocess better to ensure it does what it's supposed to
+    _ = DeployComposeStage.run(mock_config('deploy'), mock_webhook, MOCK_OUTPUT)
+
+    mock_subprocess.assert_called_once()
+
+
+@mock.patch('harvey.utils.Utils.kill')
+@mock.patch('subprocess.check_output', side_effect=subprocess.TimeoutExpired(cmd=subprocess.check_output, timeout=0.1))  # noqa
+def test_deploy_compose_stage_subprocess_timeout(mock_subprocess, mock_utils_kill, mock_project_path, mock_webhook):  # noqa
+    _ = DeployComposeStage.run(mock_config('deploy'), mock_webhook, MOCK_OUTPUT)
+
+    mock_utils_kill.assert_called_once()
+
+
+@mock.patch('harvey.utils.Utils.kill')
+@mock.patch('subprocess.check_output', side_effect=subprocess.CalledProcessError(returncode=1, cmd=subprocess.check_output))  # noqa
+def test_deploy_compose_stage_process_error(mock_subprocess, mock_utils_kill, mock_project_path, mock_webhook):  # noqa
+    _ = DeployComposeStage.run(mock_config('deploy'), mock_webhook, MOCK_OUTPUT)
+
+    mock_utils_kill.assert_called_once()
+
+
+@mock.patch('subprocess.check_output')
+def test_deploy_compose_stage_custom_compose_success(mock_subprocess, mock_webhook):
+    """This test simulates having a custom compose command set in the Harvey config
+    file - using two docker-compose files for instance
+    """
+    # TODO: Mock the subprocess better to ensure it does what it's supposed to
+    config = mock_config('deploy', compose='docker-compose.yml -f docker-compose-prod.yml')
+    _ = DeployComposeStage.run(config, mock_webhook, MOCK_OUTPUT)
+
+    mock_subprocess.assert_called_once()
