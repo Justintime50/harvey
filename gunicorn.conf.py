3e124460c812e792150c09df998b8a6e7dc070d2
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-10-25 00:21:59 -0600

    fix: allow gunicorn to be configured by env variables

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,11 @@
+from dotenv import load_dotenv
+import os
+
 # Additional details on configuring Gunicorn can be found here:
 # https://github.com/benoitc/gunicorn/blob/master/examples/example_config.py
 
-bind = '127.0.0.1:5000'
+load_dotenv()
+harvey_host = os.getenv('HOST', '127.0.0.1')
+harvey_port = os.getenv('PORT', '5000')
+
+bind = f'{harvey_host}:{harvey_port}'
