89682e28256e06be7a70eabc56d8d6e3da58687d
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-12-13 15:23:45 -0700

    fix: webhook debug message

--- harvey/api.py
+++ harvey/api.py
@@ -73,6 +73,8 @@ class Api:
         signature = request.headers.get('X-Hub-Signature-256')
 
         if payload_json:
+            logger.debug(f'Webhook received for: {Webhook.repo_full_name(payload_json)}')
+
             # The `ref` field from GitHub looks like `refs/heads/main`, so we split on the final
             # slash to get the branch name and check against the user-allowed list of branches.
             branch_name = payload_json['ref'].rsplit('/', 1)[-1]
