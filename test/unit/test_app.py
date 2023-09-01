c76e6222b072a5be27b9f481be5039ea3df3f185
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2023-09-01 15:06:29 -0600

    tests: add remaining endpoint tests

--- test/unit/test_app.py
+++ test/unit/test_app.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 
 
@@ -8,8 +10,10 @@ import pytest
         'deployments',
         'deployments/mock-deployment-id',
         'projects',
+        'projects/mock-project-name/webhook',
         'locks',
         'locks/mock-project-name',
+        'threads',
     ],
 )
 def test_routes_are_reachable_get(mock_client, route):
@@ -20,9 +24,7 @@ def test_routes_are_reachable_get(mock_client, route):
 
 @pytest.mark.parametrize(
     'route',
-    [
-        'deploy',
-    ],
+    ['deploy', 'projects/mock-project-name/redeploy'],
 )
 def test_routes_are_reachable_post(mock_client, route):
     response = mock_client.post(route)
@@ -33,6 +35,8 @@ def test_routes_are_reachable_post(mock_client, route):
 @pytest.mark.parametrize(
     'route',
     [
+        'projects/mock-project-name/lock',
+        'projects/mock-project-name/unlock',
         'locks/mock-project-name/enable',
         'locks/mock-project-name/disable',
     ],
@@ -47,3 +51,11 @@ def test_routes_not_found(mock_client):
     response = mock_client.get('bad_route')
 
     assert response.status_code == 404
+
+
+@patch('harvey.config.Config.webhook_secret', '123')
+def test_routes_not_authorized(mock_client):
+    """We have a secret set but don't pass one resulting in a not authorized response."""
+    response = mock_client.get('locks')
+
+    assert response.status_code == 401
